8b77faded3e2e319fa2b1692bcecbfd3adca6759
atidivya <atidivya@gmail.com>
2018-11-27 18:06:30 +0000

    Updates

---
+++ precache-manifest.4b6d467ed595df64ce366954f2129872.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f75cce774bcc528c2ae",
+    "url": "/reactlyricfinder/static/js/runtime~main.2f75cce7.js"
+  },
+  {
+    "revision": "a1f0e69d32414456ed85",
+    "url": "/reactlyricfinder/static/js/main.a1f0e69d.chunk.js"
+  },
+  {
+    "revision": "3cacb80bffe45e3892e9",
+    "url": "/reactlyricfinder/static/js/1.3cacb80b.chunk.js"
+  },
+  {
+    "revision": "a1f0e69d32414456ed85",
+    "url": "/reactlyricfinder/static/css/main.9743dcc1.chunk.css"
+  },
+  {
+    "revision": "e500ccdf300c99b977f94e6d215ed843",
+    "url": "/reactlyricfinder/index.html"
+  }
+];
